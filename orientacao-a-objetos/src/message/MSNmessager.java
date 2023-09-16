8139d12097cc81b55d57a18785c6fd3e653c7998
evan <evandromariano49@gmail.com>
2023-09-16 08:53:07 -0300

    2023-09-16 08:52 - Orientação a objeto

---
+++ orientacao-a-objetos/src/message/MSNmessager.java
@@ -0,0 +1,19 @@
+package message;
+
+public class MSNmessager {
+    public void enviarmensagem() {
+        System.out.println("Enviando mensagem");
+    }
+
+    public void recebermensagem() {
+        System.out.println("Recebendo mensagem");
+    }
+
+    public void validarConectadoInternet() {
+        System.out.println("Validando se está conectado na internet");
+    }
+
+    public void salvarHistoricoMenssagme() {
+        System.out.println("Salvando histórico da mensagem");
+    }
+}
